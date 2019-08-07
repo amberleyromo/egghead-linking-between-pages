8acc64a0cebc6e52b56e1854f2a724b5cff8a0c7
Amberley Romo <amberleyjohanna@gmail.com>
2019-08-07 10:03:12 -0500

    finished example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => (
   <div style={{ color: `purple` }}>
     <h1>Linking between pages in Gatsby</h1>
+    <Link to="/contact/">Contact</Link>
     <p>What a world.</p>
   </div>
 )
